fd45c60444fb8f940474503ae6b2a673aff9e2e4
Zlata839 <161966619+Zlata839@users.noreply.github.com>
2024-06-01 22:23:30 +0600

    Update Task1.java

--- Hometask.Seminar1/Task1.java
+++ Hometask.Seminar1/Task1.java
@@ -41,3 +41,33 @@ public class Printer{
         System.out.println(itresume_res);
     }
 }
+
+Решение на платформе
+
+class Answer {
+    public int countNTriangle(int n){
+if (n < 1) return -1;
+    int sum = 0;
+    for(int i = 1; i <= n; i++){
+          sum += i;
+    }
+    return sum;
+    }
+}
+
+public class Printer{ 
+    public static void main(String[] args) { 
+      int n = 0;
+
+      if (args.length == 0) {
+        n = 4;
+      }
+      else{
+        n = Integer.parseInt(args[0]);
+      }     
+
+      Answer ans = new Answer(); 
+      int itresume_res = ans.countNTriangle(n);     
+      System.out.println(itresume_res);
+    }
+}
