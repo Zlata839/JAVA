71c9821c8dcdab2b8c5a1ca687631402a4a4caca
Zlata839 <161966619+Zlata839@users.noreply.github.com>
2024-06-01 22:24:52 +0600

    Update Task3.java

--- Hometask.Seminar1/Task3.java
+++ Hometask.Seminar1/Task3.java
@@ -64,3 +64,76 @@ public class Printer{
         System.out.println(result);
     }
 }
+
+// Решение платформы
+
+class Calculator {
+    public double calculate(char op, int a, int b) {
+        // Введите свое решение ниже
+        double result = 0;
+        switch (op) {
+            case '+':
+                result = add(a, b);
+                break;
+            case '-':
+                result = minus(a, b);
+                break;
+            case '*':
+                result = mult(a, b);
+                break;
+            case '/':
+                result = divide(a, b);
+                break;
+        }
+        return result;
+    }
+
+    private double divide(int a, int b) {
+        if (b != 0)
+            return (double) a / b;
+        return -1;
+    }
+
+    private int mult(int a, int b) {
+        return a * b;
+    }
+
+    private int minus(int a, int b) {
+        return a - b;
+    }
+
+    private int add(int a, int b) {
+        return a + b;
+    }
+}
+
+// Не удаляйте этот класс!
+// Он нужен для вывода результатов на экран и проверки
+public class Printer {
+    public static void main(String[] args) {
+        int a = 0;
+        char op = ' ';
+        int b = 0;
+
+        if (args.length == 0) {
+            // При отправке кода на Выполнение, вы можете варьировать эти параметры
+            a = 7;
+            op = '+';
+            b = 3;
+        } else {
+            a = Integer.parseInt(args[0]);
+            op = args[1].charAt(0);
+            b = Integer.parseInt(args[2]);
+        }
+
+        // Ниже добавить проверку оператора на корректность
+        if (op != '+' && op != '-' && op != '*' && op != '/') {
+            System.out.println("Некорректный оператор: " + op);
+            return;
+        }
+
+        Calculator calculator = new Calculator();
+        double result = calculator.calculate(op, a, b);
+        System.out.println(result);
+    }
+}
